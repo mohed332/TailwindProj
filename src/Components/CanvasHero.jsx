3d8322931d217d99d136118f98e7ee985281ddef
mohed332 <mohed332@gmail.com>
2023-08-24 23:58:16 +0200

    random addons

--- src/Components/CanvasHero.jsx
+++ src/Components/CanvasHero.jsx
@@ -1,17 +1,37 @@
-import { OrbitControls, PerspectiveCamera, RenderTexture, Text } from '@react-three/drei'
+import { ContactShadows, OrbitControls, PerspectiveCamera, RenderTexture, ScrollControls, Text } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import CubeModel from './CubeModel'
+
 
 const CanvasHero = () => {
   return (
     <> 
         <div className='w-screen h-screen'>
         <Canvas camera={{fov: 25, position:[8,8,8]}}>
-        <OrbitControls enableZoom={false} autoRotate  />
-        <ambientLight intensity={5}/>
-        <directionalLight position={[3,2,1]}/>
+        <ambientLight intensity={2}/>
+        <ScrollControls pages={3} damping={0.25}>
+
+        <directionalLight position={[10,10,5]}/>
             <CubeModel/>
+        <OrbitControls 
+        enableZoom={false} 
+        minPolarAngle={Math.PI / 10} 
+        maxPolarAngle={Math.PI /2 } />
+        <ContactShadows 
+        frames={1} 
+        position={[0, -0.5, 0]} 
+        blur={1} 
+        opacity={1}
+        />
+        <ContactShadows 
+        frames={1} 
+        position={[0, -0.5, 0]} 
+        blur={3} 
+        opacity={1}
+        color={'red'}
+        />
+        </ScrollControls>
         </Canvas>
         </div>
     </>
--- src/Components/CubeModel.jsx
+++ src/Components/CubeModel.jsx
@@ -1,27 +1,44 @@
 import { PerspectiveCamera, RenderTexture, Text } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import React, { useRef } from 'react'
+import gasp from 'gasp';
+import React, { useLayoutEffect, useRef } from 'react'
 
 const CubeModel = () => {
     const textRef = useRef();
-    useFrame(state => (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2))
+    useFrame(state => (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 0.5))
+    const refBlock = useRef();
+    const tl = useRef();
+    useLayoutEffect(() => {
+      tl.current = gasp.timeline();
+
+      tl.current.to(
+        refBlock.current.position,
+      )
+    }, [])
   return (
     <>
+      <group {...props} dispose={null} ref={refBlock}>
+
         <mesh>
                 <boxGeometry args={[2,2,2]} />
                 <meshStandardMaterial>
-                  <RenderTexture attach='map'>
+                  <RenderTexture attach='map' anisotropy={16}>
                   <PerspectiveCamera
                     makeDefault
-                    position={[0,0,3]}
+                    position={[0,0,5]}
+                    manual
+                    aspect={1 / 3}
                   />
-                  <color attach='background' args={['magenta']}/>
-                    <Text ref={textRef} fontSize={1} color={"red"}>
+                  <color attach='background' args={['orange']}/>
+                  <ambientLight intensity={0.5}/>
+                  <directionalLight position={[10,10,5]}/>
+                    <Text ref={textRef} fontSize={1} color={"purple"}>
                       Hello
                     </Text>
                   </RenderTexture>
                 </meshStandardMaterial>
             </mesh>
+      </group>
     </>
   )
 }
